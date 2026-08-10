f8d7a9f4cbfcae91b4a70a2fe85a55510ac4788c
Maulana Ahmad Aji Triadi <maulanaatriadi@gmail.com>
2026-08-11 00:08:07 +0700

    keymap: move pasteOpen commands behind the g prefix

--- content/keymap.js
+++ content/keymap.js
@@ -61,8 +61,8 @@
 
     // Clipboard
     Y: 'copyTitleUrl',
-    p: 'pasteOpen',
-    P: 'pasteOpenBackground',
+    gp: 'pasteOpen',
+    gP: 'pasteOpenBackground',
 
     // Site-level control
     I: 'toggleIgnore',
